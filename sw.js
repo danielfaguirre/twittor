b7d52eac89fbc8dd6c2c35f3e911a5d19c865390
Daniel <danielfernaco@gmail.com>
2020-08-21 16:38:59 -0500

    Prevent pull to refresh

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 //imports
-importScripts ('js/sw-utils.js');
+importScripts('js/sw-utils.js');
 
 const STATIC_CACHE = 'static-v1';
-const DYNAMIC_CACHE = 'dynamic-v1';
+const DYNAMIC_CACHE = 'dynamic-v2';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 const APP_SHELL = [
@@ -11,7 +11,7 @@ const APP_SHELL = [
     'css/style.css',
     'css/animate.css',
     'img/favicon.ico',
-    'img/avatars/spiderman.jpg', 
+    'img/avatars/spiderman.jpg',
     'img/avatars/ironman.jpg',
     'img/avatars/wolverine.jpg',
     'img/avatars/thor.jpg',
@@ -45,25 +45,25 @@ self.addEventListener('activate', e => {
             keys.map(key => {
                 if (key !== STATIC_CACHE && key.includes('static'))
                     return caches.delete(key);
+
+                if (key !==DYNAMIC_CACHE && key.includes('dynamic'))
+                    return caches.delete(key);
             });
         });
-    e.waitUntil(resp);   
+    e.waitUntil(resp);
 });
 
 self.addEventListener('fetch', event => {
     const cacheResp = caches.match(event.request)
-        .then(resp=>{
-            if(resp){
-                console.log('Found', resp);
-                return resp;
-            }else{
-                console.log('not found', resp);
+        .then(resp => {
+            if (resp) return resp
+            else {
                 return fetch(event.request)
-                    .then(newResp=>{ 
+                    .then(newResp => {
                         return actualizarcacheDinamico(DYNAMIC_CACHE, event.request, newResp);
-                    });                   
+                    });
             }
         });
     event.respondWith(cacheResp);
-    
+
 });
